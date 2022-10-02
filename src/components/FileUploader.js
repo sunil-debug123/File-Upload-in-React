c07e5d044cc551d95a59ab63c378fafbcd6c2f06
Sunil <sunil31725@gmail.com>
2022-10-02 09:51:47 -0700

    working

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FileUploader() {
+    const [selectedImage, setselectedImage] = useState();
   const handleChange = (e) => {
-    console.log(e.target.files[0]);
+    console.log(e.target.files.length);
+    if (e.target.files && e.target.files.length > 0) {
+        setselectedImage(e.target.files[0])
+    }
   };
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
   return (
     <div>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label for="formFileLg" className="form-label">
             File Upload in React js 📁
           </label>
@@ -15,13 +22,17 @@ export default function FileUploader() {
             className="form-control form-control-lg"
             id="formFileLg"
             type="file"
+            accept="image"
             onChange={handleChange}
           />
-          <button type="button" className="mt-3 px-4 btn btn-success">
+          <button type="submit" className="mt-3 px-4 btn btn-success">
             Upload
           </button>
         </form>
       </div>
+      {selectedImage && (
+        <img src={URL.createObjectURL(selectedImage)} class="img-thumbnail" alt="..."></img>
+      )}
     </div>
   );
 }
